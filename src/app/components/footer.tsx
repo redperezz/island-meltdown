4efcc082d0bff8ec684b64856c345c76661273e7
Oakinite <kaicruzz91@gmail.com>
2024-09-10 12:27:04 -0700

    init

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <div>
+      <div>
+        <p>Sign up for our Newsletter!</p>
+      </div>
+      <div>
+        <ul>
+          <li>Instagram</li>
+          <li>Facebook</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,13 @@
+export default function NavBar() {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>Menu</li>
+        <li>Calendar</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+}
