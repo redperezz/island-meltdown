f55bb8b6e81e3b281b82149d1f47f185aa0b4157
Oakinite <kaicruzz91@gmail.com>
2024-09-18 16:41:45 -0700

    Start working on menu hamburger

--- src/app/components/navmenu.tsx
+++ src/app/components/navmenu.tsx
@@ -1,43 +1,75 @@
+'use client';
 import Link from 'next/link';
 import classNames from 'classnames';
 import styles from './navmenu.module.css';
+import { useEffect } from 'react';
 
 export default function NavMenu() {
+  useEffect(() => {
+    const hamburger = document.querySelector('.hamburger');
+    const navMenu = document.querySelector('.navMenu');
+
+    hamburger?.addEventListener('click', () => {
+      hamburger.classList.toggle('active');
+      navMenu?.classList.toggle('active');
+    });
+
+    document.querySelectorAll('.navLink').forEach((n) =>
+      n.addEventListener('click', () => {
+        hamburger?.classList.remove('active');
+        navMenu?.classList.remove('active');
+      })
+    );
+  }, []);
+
   return (
     <>
-      <ul className={styles.navMenu}>
-        <li className={styles.navItem}>
-          <Link href='../' className={styles.navLink}>
+      <ul className={classNames('navMenu', styles.navMenu)}>
+        <li className={classNames('navItem', styles.navItem)}>
+          <Link href='../' className={classNames('navLink', styles.navLink)}>
             Home
           </Link>
         </li>
-        <li className={styles.navItem}>
-          <Link href='../menu' className={styles.navLink}>
+        <li className={classNames('navItem', styles.navItem)}>
+          <Link
+            href='../menu'
+            className={classNames('navLink', styles.navLink)}
+          >
             Menu
           </Link>
         </li>
-        <li className={styles.navItem}>
-          <Link href='../calendar' className={styles.navLink}>
+        <li className={classNames('navItem', styles.navItem)}>
+          <Link
+            href='../calendar'
+            className={classNames('navLink', styles.navLink)}
+          >
             Calendar
           </Link>
         </li>
-        <li className={styles.navItem}>
-          <Link href='../about' className={styles.navLink}>
+        <li className={classNames('navItem', styles.navItem)}>
+          <Link
+            href='../about'
+            className={classNames('navLink', styles.navLink)}
+          >
             About
           </Link>
         </li>
-        <li className={styles.navItem}>
-          <Link href='' className={styles.navLink}>
+        <li className={classNames('navItem', styles.navItem)}>
+          <Link href='' className={classNames('navLink', styles.navLink)}>
             Contact
           </Link>
         </li>
-        <li className={classNames(styles.navItem, styles.findUs)}>
-          <Link href='' className={styles.navLink}>
+        <li className={classNames('navItem', styles.navItem, styles.findUs)}>
+          <Link href='' className={classNames('navLink', styles.navLink)}>
             Find Us
           </Link>
         </li>
       </ul>
-      <div></div>
+      <div className={classNames('hamburger', styles.hamburger)}>
+        <span className={classNames('bar', styles.bar)}></span>
+        <span className={classNames('bar', styles.bar)}></span>
+        <span className={classNames('bar', styles.bar)}></span>
+      </div>
     </>
   );
 }
