7c9e29d51ce15a853b4234ff213b2fd3aed2aad7
Oakinite <kaicruzz91@gmail.com>
2024-09-19 14:39:01 -0700

    Add Fredoka font and fix letter spacing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,8 @@
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
+import { fredoka } from './fonts/fonts';
 import './globals.css';
 import Header from './components/header';
 import Footer from './components/footer';
-
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -26,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang='en' className={fredoka.className}>
+      <body>
         <Header />
         {children}
         <Footer />
